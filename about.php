9714997e67a448c35187bbe248b19e4fbd1a9e5c
Shayna <shaynak112@gmail.com>
2019-03-03 16:43:52 -0500

    Add files via upload
    original

---
+++ about.php
@@ -0,0 +1,163 @@
+<!-- ABOUT -->
+  <section id="about">
+    <div class="container flex">
+
+      <div class="header waypoint" data-animation="slide-in-left">ABOUT</div>
+      <div class="header-bar waypoint" data-animation="slide-in-left" data-delay=".5s"></div>
+
+      <div class="flex row label-wrap">
+        <div class="flex row-gt-sm">
+        <div class="flex bullet-wrap ">
+          <div class="hex-wrap waypoint" data-animation="flip-in-x">
+            <div class="hexagon">
+              <i class="mdi mdi-speedometer"></i>
+            </div>
+          </div>
+          <div class="waypoint" data-animation="fade-in">
+            <div class="label bold">PHP</div>
+            <div>Primarily working with PHP.</div>
+          </div>
+        </div>
+
+        <div class="flex bullet-wrap " >
+          <div class="hex-wrap waypoint" data-animation="flip-in-x" data-delay=".2s">
+            <div class="hexagon">
+              <i class="mdi mdi-cellphone-link"></i>
+            </div>
+          </div>
+          <div class="waypoint" data-animation="fade-in" data-delay=".2s">
+            <div class="label bold">Responsive</div>
+            <div>Projects will work on mobile, computers, tablets, etc.</div>
+          </div>
+        </div>
+        </div>
+        <div class="flex row-gt-sm">
+          <div class="flex bullet-wrap " >
+            <div class="hex-wrap waypoint" data-animation="flip-in-x" data-delay=".4s">
+              <div class="hexagon">
+                <i class="mdi mdi-lightbulb-outline"></i>
+              </div>
+            </div>
+            <div class="waypoint" data-animation="fade-in" data-delay=".4s">
+              <div class="label bold">APIs</div>
+              <div>Work with various APIs such as social media.</div>
+            </div>
+          </div>
+
+          <div class="flex bullet-wrap " >
+            <div class="hex-wrap waypoint" data-animation="flip-in-x" data-delay=".6s">
+              <div class="hexagon">
+                <i class="mdi mdi-rocket"></i>
+              </div>
+            </div>
+            <div class="waypoint" data-animation="fade-in" data-delay=".6s">
+              <div class="label bold">Javascript</div>
+              <div>Experience with Javascript and JQuery</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="skills-wrapper flex row-gt-sm">
+        <div class="flex flex-50-gt-sm waypoint" data-animation="slide-in-left">
+          <!--<img src="img/me.png" class="me">-->
+          <!-- <div><img class="up-arrow" src="img/up-arrow.png"></div> -->
+          <div class="label bold">Who's this web developer?</div>
+          <div>
+            Write something.....I'm the Front-End Developer for <a class="highlight" href="http://www.eyecuelab.com" target="_blank">EyeCue Lab</a> in Portland, OR.
+            <br>
+            I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.
+            <br>
+            <span class="page-link highlight" dest="contact">Let's make something special.</span>
+          </div>
+        </div>
+
+        <div class="flex flex-50-gt-sm waypoint bars-wrap"  data-animation="slide-in-right">
+
+          <h2>Experience</h2>
+
+          <ul>
+            <li>PHP</li>
+            <li>Javascript</li>
+            <li>MySQL</li>
+
+          </ul>
+
+
+          <!--
+          <div class="bar flex">
+            <div class="bar fill" style="width:75%">
+              <div class="tag bold flex">CSS</div>
+            </div>
+            <span>80%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:85%">
+              <div class="tag bold flex">HTML</div>
+            </div>
+            <span>90%</span>
+          </div>
+
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:65%">
+              <div class="tag bold flex">React</div>
+            </div>
+            <span>70%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:80%">
+              <div class="tag bold flex">JavaScript</div>
+            </div>
+            <span>80%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:65%">
+              <div class="tag bold flex">Angular</div>
+            </div>
+            <span>70%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:55%">
+              <div class="tag bold flex">Node.js</div>
+            </div>
+            <span>50%</span>
+          </div>
+
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:65%">
+              <div class="tag bold flex">Ruby/Rails</div>
+            </div>
+            <span>70%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:55%">
+              <div class="tag bold flex">UI Design</div>
+            </div>
+            <span>50%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:60%">
+              <div class="tag bold flex">Photoshop</div>
+            </div>
+            <span>60%</span>
+          </div>
+
+          <div class="bar flex">
+            <div class="bar fill" style="width:45%">
+              <div class="tag bold flex">Illustrator</div>
+            </div>
+            <span>40%</span>
+          </div>
+
+        </div>-->
+      </div>
+    </div>
+  </section>
---
+++ portfolio.php
@@ -0,0 +1,92 @@
+<!-- PORTFOLIO -->
+  <section class="flex" id="portfolio">
+
+    <div class="header waypoint" data-animation="slide-in-right">PROJECTS</div>
+    <div class="header-bar waypoint" data-animation="slide-in-right" data-delay=".3s"></div>
+
+    <div class="filter-wrap waypoint" data-animation="fade-in">
+      <div class="flex row">
+        <div class="filter" data-filter="all">ALL</div>
+        <div class="filter" data-filter=".php">PHP</div>
+        <div class="filter" data-filter=".mysql">MYSQL</div>
+        <div class="filter" data-filter=".apis">APIs</div>
+      </div>
+      <div class="float-bar">
+        <div class="flex row">
+          <div class="filter" data-filter="all">ALL</div>
+          <div class="filter" data-filter=".php">PHP</div>
+          <div class="filter" data-filter=".mysql">MYSQL</div>
+          <div class="filter" data-filter=".apis">APIs</div>
+        </div>
+      </div>
+    </div>
+
+    <div id="gallery" class="container flex row wrap waypoint">
+      <div class="mix php apis mysql" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">Gaia Toronto</div>
+            <span class="highlight">PHP, MySQL, JavaScript, APIs, CSS, social media</span>
+          </div>
+          <div class="button" id="gaia">LEARN MORE</div>
+        </div>
+      </div>
+
+      <div class="mix php apis" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">Remember Your Umberlla</div>
+            <span class="highlight">APIs, PHP, CSS, Javascript, XML, Materialize</span>
+          </div>
+          <div class="button" id="ryu">LEARN MORE</div>
+        </div>
+      </div>
+
+      <div class="mix php apis mysql" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">Point Entertainment</div>
+            <span class="highlight">PHP, MySQL, JavaScript, APIs, social media, CSS</span>
+          </div>
+          <div class="button" id="point">LEARN MORE</div>
+        </div>
+      </div>
+
+      <div class="mix php mysql" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">Voxweb</div>
+            <span class="highlight">PHP, AJAX, JavaScript, MySQL, CSS</span>
+          </div>
+          <div class="button" id="vox">LEARN MORE</div>
+        </div>
+      </div>
+
+      <div class="mix php mysql apis" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">MUZE</div>
+            <span class="highlight">PHP, MySQL, JavaScript, CSS, APIs, social media</span>
+          </div>
+          <div class="button" id="muze">LEARN MORE</div>
+        </div>
+      </div>
+
+
+      <div class="mix php mysql" data-my-order="1">
+        <div>
+          <div class="card"></div>
+          <div class="text">
+            <div class="bold">David Junk</div>
+            <span class="highlight">PHP, MySQL, JavaScript, CSS, Lightbox Library</span>
+          </div>
+          <div class="button" id="davidjunk">LEARN MORE</div>
+        </div>
+      </div>
+    </div>
+  </section>
